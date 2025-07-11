286fd9a3fa15083db8087b6a0979123932da2e69
tugbabostanci <ttugbabostanci@gmail.com>
2025-07-11 13:58:37 +0300

    tas2 rev

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function getShippingMessage(country, price, deliveryFee){
-    return `Shipping to ${"country"} will cost ${price * deliveryFee} credits!`;
+    return `Shipping to ${country} will cost ${price + deliveryFee} credits!`;
 }
 
 
